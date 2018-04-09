56b202c2b8de00c0ddb5a1d096cbbe48d81ba5ac
Piyush Kumar <kpiyush1709@gmail.com>
2018-04-10 00:36:39 +0530

    added basic routes and set handlebars middleware as template engine

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: main }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 app.listen(PORT, () => {
   console.log('Server started');
