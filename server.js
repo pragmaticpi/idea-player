0c627b4d29d394cf2891b692c34d2a012de1f218
Piyush Kumar <kpiyush1709@gmail.com>
2018-04-12 21:42:32 +0530

    routes handled with extra dependencies

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -27,7 +30,23 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+
+  next();
+})
 // Index Route
 app.get('/', (req, res) => {
   res.render('index');
@@ -38,12 +57,32 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+app.get('/ideas', (req, res) => {
+  ideaSchema.find({})
+    .sort({date:'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas,
+      });
+    });
+});
+
+app.get('/ideas/edit/:id', (req, res) => {
+  ideaSchema.findOne({
+    _id: req.params.id,
+  })
+  .then(idea => {
+    res.render('ideas/edit', {
+      idea,
+    });
+  });
+});
+
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
 });
 
 app.post('/ideas', (req, res) => {
-  console.log(req.body.title);
 
   // Manual server-side validation
   let errors = [];
@@ -62,9 +101,45 @@ app.post('/ideas', (req, res) => {
       details: req.body.details,
     });
   } else {
-    res.send("OK");
+    const newIdea = {
+      title: req.body.title,
+      details: req.body.details,
+    };
+
+    new ideaSchema(newIdea)
+      .save()
+      .then(idea => {
+        req.flash('success_msg', 'Idea Added');
+        res.redirect('/ideas');
+      });
   }
 });
+
+app.put('/ideas/:id', (req, res) => {
+  ideaSchema.findOne({
+    _id: req.params.id,
+  })
+  .then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save()
+      .then(idea => {
+        req.flash('success_msg', 'Idea Updated');
+        res.redirect('/ideas');
+      });
+  });
+});
+
+app.delete('/ideas/:id', (req, res) => {
+  ideaSchema.deleteOne({
+    _id: req.params.id,
+  })
+  .then(() => {
+    req.flash('success_msg', 'Idea Removed');
+    res.redirect('/ideas');
+  })
+})
 // Server start
 app.listen(PORT, () => {
   console.log('Server started');
